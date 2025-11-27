c51f84c74d98e37bb7c6e7381810d1c708133553
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2025-11-27 23:00:36 +0400

    feat: icon colors based on active state (red/black)

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -17,37 +17,53 @@ interface ToolbarProps {
 
 function Toolbar({ viewMode, setViewMode, products }: ToolbarProps) {
   return (
-    <div className="flex items-center justify-between bg-gray-50 pl-1 pr-3 py-4 z-20 sticky top-[112.2px] lg:top-[136px]">
+    <div className="flex items-center justify-between bg-gray-50 pl-2 pr-4 py-4 z-20 mt-29 lg:mt-0 lg:sticky lg:top-[136px]">
       <div className="flex items-center gap-2">
         <Button
-          variant={viewMode === "list" ? "default" : "ghost"}
-          size="icon"
+          variant={viewMode === "list" ? "secondary" : "ghost"}
+          size="icon-sm"
           onClick={() => setViewMode("list")}
         >
-          <Menu className="w-5 h-5" />
+          <Menu
+            className={`w-5 h-5 ${
+              viewMode === "list" ? "text-red-500" : "text-black"
+            }`}
+          />
         </Button>
         <Button
-          variant={viewMode === "grid" ? "default" : "ghost"}
-          size="icon"
+          variant={viewMode === "grid" ? "secondary" : "ghost"}
+          size="icon-sm"
           onClick={() => setViewMode("grid")}
         >
-          <LayoutGrid className="w-5 h-5" />
+          <LayoutGrid
+            className={`w-5 h-5 ${
+              viewMode === "grid" ? "text-red-500" : "text-black"
+            }`}
+          />
         </Button>
-        <span className="ml-2 text-sm">
+        <span className="ml-2 text-xs hidden sm:inline-flex">
           There are {products.length} products.
         </span>
       </div>
       <div className="flex items-center gap-2">
-        <span className="text-sm">Sort By</span>
+        <span className="text-xs hidden sm:flex">Sort By</span>
         <Select defaultValue="name-asc">
-          <SelectTrigger className="w-40">
+          <SelectTrigger className="w-34">
             <SelectValue />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="name-asc">Name, A To Z</SelectItem>
-            <SelectItem value="name-desc">Name, Z To A</SelectItem>
-            <SelectItem value="price-asc">Price, Low to High</SelectItem>
-            <SelectItem value="price-desc">Price, High to Low</SelectItem>
+            <SelectItem value="name-asc" className="text-xs">
+              Name, A To Z
+            </SelectItem>
+            <SelectItem value="name-desc" className="text-xs">
+              Name, Z To A
+            </SelectItem>
+            <SelectItem value="price-asc" className="text-xs">
+              Price, Low to High
+            </SelectItem>
+            <SelectItem value="price-desc" className="text-xs">
+              Price, High to Low
+            </SelectItem>
           </SelectContent>
         </Select>
       </div>
