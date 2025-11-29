a719331075cbfbe285f9f4ded7b807198c6d398a
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2025-11-30 00:17:14 +0400

    Initial commit

--- app/(dashboard)/products/page.tsx
+++ app/(dashboard)/products/page.tsx
@@ -1,11 +1,25 @@
-"use client";
+// import { ProductGrid } from "@/components/ProductGrid";
 
+// function page() {
+//   return (
+//     <div className="flex-1 min-w-0">
+//       <ProductGrid />
+//     </div>
+//   );
+// }
+
+// export default page;
+// /app/(dashboard)/products/page.tsx
+
+import { Suspense } from "react"; // 👈 დაამატეთ ეს იმპორტი
 import { ProductGrid } from "@/components/ProductGrid";
 
 function page() {
   return (
     <div className="flex-1 min-w-0">
-      <ProductGrid />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProductGrid />
+      </Suspense>
     </div>
   );
 }
